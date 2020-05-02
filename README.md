a7af1eb2a892e0a7c93349097ef05633d1fc99bb
JiaruiFeng <feng.jiarui@wustl.edu>
2020-05-02 15:00:41 -0500

    update some scripts and readme

--- README.md
+++ README.md
@@ -1,3 +1,83 @@
 # CIA-Price-Recommendation-System
 This project is for ESE529 course project, Washington University in St.Louis.  We build a online secondary product price recommendation system build on Kaggle's Mercari dataset, the website for dataset and description is in [Kaggle Competition](https://www.kaggle.com/c/mercari-price-suggestion-challenge/overview).
 
+### 1. introduction
+
+You can view the final system through. Based on the dataset, we use machine learning and deep learning technique to construct prediction workflow to transform and predict price using information user provide with us. In the system, you can input the information about your product, like the name, brand, category, description, etc. Then, system will give you a reasonable price based on information
+
+### 2. Model Construction Workflow
+
+#### 2.1 Dataset
+
+We describe dataset first. For the training dataset,there are total six predictive variables in data and one response variable:
+  `name`: the name of the item.
+  `item_condition_id`: an integer specify the condition of item. There are total five condition:1,2,3,4,5.
+  `category_name`: a string describe the category of item.
+  `brand name`: a string describe the brand of item.
+  `shipping`: 1 if shipping fee is paid by seller and 0 by buyer.
+  `item_description`: a sectence describe the item inputed by seller.
+  `price`: the price that the item was sold for.
+ There are total 1482535 data in train dataset.  For preprocessing, we split the variable `category_name` to three level called `category1`, `category2` and `category3` respectively. 
+
+![preprocessing](application/picture/preprocessing.png)
+
+#### 2.2 Feature Construction
+
+For the feature construction, in order to increase the accuracy of system, we implement ensemble idea by construct different type of features and use these feature to build model respectively. Then, we combine all the model and give final prediction. To be more specific, we construct four type of model, named final, logical, stem and combine respectively. below are how we construct them:
+
+##### Final feature
+
+![final_feature](application/picture/final_feature.png)
+
+##### Logical Feature
+
+![logical_feature](application/picture/logical_feature.png)
+
+##### Stem Feature
+
+![stem_feature](application/picture/stem_feature.png)
+
+##### Combine Feature
+
+![combine_feature](application/picture/combine_feature.png)
+
+Actually, when bulid the model, we found that the final feature would give us best performance among four type of feature. 
+
+For auto encoder, the structure is follow:
+
+![auto_ encoder_structure](application/picture/auto_ encoder_structure.png)
+
+#### 2.3 Model Construction
+
+For model construction, we first use final feature to evaluate different model and parameters. We test Random Forest, XGBoost, DNN and CNN. Evaluation strategy is follow: we divide total training dataset to new training and new test dataset with ratio 0.7/0.3. Then, we construct model and do the 5-folds cross validation on the new training dataset. For evaluation metric, we use Root Mean Squared Logarithmic Error(RMSLE), the formula is follow:
+$$
+\epsilon=\sqrt{\frac{1}{n} \sum_{i=1}^{n}\left(\log \left(p_{i}+1\right)-\log \left(a_{i}+1\right)\right)^{2}}
+$$
+Where $\epsilon$ is the total error, $n$ is the total number ofobservations, $p_i$ is the prediction value for observation $i$, $a_i$ is the true value.
+
+Finally we choose the XGBoost and CNN as our final model. Actually DNN and CNN have best performance in cross validation. However, the size of deep learning model is much larger than model like XGBoost. Thus, for model construction, we finally train four XGBoost model use four type of features. Meanwhile, to further increase the accuracy, we train a single CNN model use final feature. The structure of CNN is follow:
+
+![CNN_structure](application/picture/CNN_structure.png)
+
+Finally, we combine result of five model prediction with another XGBoost to get our final prediction of price.  For the final XGBoost, we use all the data in new training dataset to train five model, then we use the preidiction for new test dataset of five model as new input data to train and evaluate XGBoost use 5-folds cross-validation.
+
+### 3. System Construction
+
+After we evaluate all the model, we finally train all the model using all the training dataset with the parameters we get in validation. These models then be embedded to the system use to predict price for new product. The workflow of system is follow:
+
+![system workflow](application/picture/system workflow.png)
+
+ 
+
+### 4. Limitation
+
+However, although we spend lot's of time try to buila a very huge and complexed system, with the wish that such a system would give us more accuracy and robust prediction for new data. However, turns out it is not the case. The performance is not very good compare to the leaderboard in Kaggle. And such a hug system make prediction procedure become slow. We think one of the reason lay on the process of data. All the single model trained on one of feature have bad performance indicate that there are some problem for feature construction. We may need spend more time to dig into it and construct feature that would have better performance even if we only use single model.
+
+
+
+### 5. Contact
+
+Jiarui Feng: jiarui.feng@wustl.edu
+
+Zhiling Zhang: zhilingzhang@wustl.edu
+
--- test_data_processing_pipeline.R
+++ test_data_processing_pipeline.R
@@ -4,7 +4,7 @@ library(stringr)
 library(text2vec)
 library(fastDummies)
 library(corpus)
-
+source("SparseMatrixFit.R")
 test_data<-read.csv("raw_data/test_stg2.tsv",sep="\t",header = T)
 test_missing_index<-which(as.character(test_data$category_name)=="")
 test_data$category_name<-as.character(test_data$category_name)
@@ -50,16 +50,6 @@ test_data$brand_name[test_brand_missing]="missing"
 test_data$item_description=as.character(test_data$item_description)
 test_data$item_description[test_data$item_description==""]="missing"
 
-
-count_length<-function(x){
-  split_desc<-unlist(strsplit(x,split=" "))
-  split_desc<-split_desc[which(!split_desc%in%c(" ",""))]
-  return(length(split_desc))
-}
-test_desc_length<-sapply(test_data$item_description,count_length)
-test_desc_length<-as.numeric(test_desc_length)
-test_data<-data.frame(test_data,desc_length=test_desc_length)
-
 remove_punctuation<-function(x){
   processed_string<-str_replace_all(x,pattern="\\[rm\\]",replacement = "")
   processed_string<-str_replace_all(processed_string,pattern="[:punct:]",replacement="")
@@ -73,9 +63,22 @@ remove_punctuation<-function(x){
   processed_string
 }
 
+test_data$item_description<-sapply(test_data$item_description,remove_punctuation)
+
+count_length<-function(x){
+  split_desc<-unlist(strsplit(x,split=" "))
+  split_desc<-split_desc[which(!split_desc%in%c(" ",""))]
+  return(length(split_desc))
+}
+test_desc_length<-sapply(test_data$item_description,count_length)
+test_desc_length<-as.numeric(test_desc_length)
+test_data<-data.frame(test_data,desc_length=test_desc_length)
+
+
+
 #other features
 test_dummy_condition<-dummy_cols(c(test_data$item_condition_id))
-test_dummy_condition<-dummy_condition[,-1]
+test_dummy_condition<-test_dummy_condition[,-1]
 colnames(test_dummy_condition)<-c("condition_id_1","condition_id_2","condition_id_3","condition_id_4","condition_id_5")
 
 test_dummy_main_cate<-dummy_cols(c(test_data$main_category))
@@ -86,13 +89,15 @@ test_dummy_sub_cate<-test_dummy_sub_cate[,-1]
 
 test_log_length<-log10(c(test_data$desc_length))
 model_test_data<-data.frame(test_dummy_condition,test_dummy_main_cate,test_dummy_sub_cate,test_data$shipping,test_log_length)
-
+save(model_test_data,file="processed_data/model_test_data.RData")
+rm(test_dummy_condition,test_dummy_main_cate,test_dummy_sub_cate,test_split_categ)
+rm(test_brand_missing,test_desc_length,test_log_length)
 
 # final Feature-------------
-final_desc_encoder<-keras:load_model_hdf5("model/description_encoder_32.h5")
-final_name_encoder<-keras:load_model_hdf5("model/name_encoder_8.h5")
-final_cate_encoder<-keras:load_model_hdf5("model/cate_encoder_8.h5")
-final_brand_encoder<-keras:load_model_hdf5("model/brand_encoder_8.h5")
+final_desc_encoder<-keras::load_model_hdf5("model/description_encoder_32.h5")
+final_name_encoder<-keras::load_model_hdf5("model/name_encoder_8.h5")
+final_cate_encoder<-keras::load_model_hdf5("model/cate_encoder_8.h5")
+final_brand_encoder<-keras::load_model_hdf5("model/brand_encoder_8.h5")
 load("model/brand_tfidf.RData")
 load("model/cate_tfidf.RData")
 load("model/desc_tfidf.RData")
@@ -104,6 +109,8 @@ load("model/name_vectorizer.RData")
 
 #name 
 test_data_name<-sapply(test_data$name,remove_punctuation)
+prep_fun = tolower
+tok_fun = space_tokenizer
 it_name_test=itoken(test_data_name, 
                     preprocessor = prep_fun, 
                     tokenizer = tok_fun, 
@@ -113,8 +120,9 @@ it_name_test=itoken(test_data_name,
 #use pre-trained tf-idf to convert test name
 dtm_test_name =create_dtm(it_name_test,name_vectorizer)
 dtm_test_name_tfidf=transform(dtm_test_name,name_tfidf)
-final_name_train_8<-predict_on_batch(final_name_encoder,dtm_test_name_tfidf)
-
+final_name_test_8<-sparseMatrixPredict(dtm_test_name_tfidf,final_name_encoder,batch_size=1024,3460725)
+save(final_name_test_8,file="processed_data/final_name_test_8.RData")
+rm(dtm_test_name,dtm_test_name_tfidf)
 
 test_data_brand<-sapply(test_data$brand_name,remove_punctuation)
 it_brand_test=itoken(test_data_brand, 
@@ -125,7 +133,10 @@ it_brand_test=itoken(test_data_brand,
 #use pre-trained tf-idf to convert test name
 dtm_test_brand =create_dtm(it_brand_test,brand_vectorizer)
 dtm_test_brand_tfidf=transform(dtm_test_brand,brand_tfidf)
-final_brand_train_8<-predict_on_batch(final_brand_encoder,dtm_test_brand_tfidf)
+final_brand_test_8<-sparseMatrixPredict(dtm_test_brand_tfidf,final_brand_encoder,batch_size=1024,3460725)
+save(final_brand_test_8,file="processed_data/final_brand_test_8.RData")
+rm(dtm_test_brand,dtm_test_brand_tfidf)
+
 
 test_data_cate<-sapply(test_data$sub_category2,remove_punctuation)
 it_cate_test=itoken(test_data_cate, 
@@ -136,11 +147,13 @@ it_cate_test=itoken(test_data_cate,
 #use pre-trained tf-idf to convert test name
 dtm_test_cate =create_dtm(it_cate_test,cate_vectorizer)
 dtm_test_cate_tfidf=transform(dtm_test_cate,cate_tfidf)
-final_cate_train_8<-predict_on_batch(final_cate_encoder,dtm_test_cate_tfidf)
+final_cate_test_8<-sparseMatrixPredict(dtm_test_cate_tfidf,final_cate_encoder,batch_size=1024,3460725)
+save(final_cate_test_8,file="processed_data/final_cate_test_8.RData")
+rm(dtm_test_cate,dtm_test_cate_tfidf)
 
-test_description<-sapply(test_data$item_description,remove_punctuation)
-test_description<-as.character(test_description)
-test_data$item_description<-test_description
+
+
+
 it_desc_test=itoken(test_data$item_description, 
                     preprocessor = prep_fun, 
                     tokenizer = tok_fun, 
@@ -150,35 +163,35 @@ it_desc_test=itoken(test_data$item_description,
 #use pre-trained tf-idf to convert test name
 dtm_test_desc =create_dtm(it_desc_test,desc_vectorizer)
 dtm_test_desc_tfidf=transform(dtm_test_desc,desc_tfidf)
-final_desc_train_32<-predict_on_batch(final_desc_encoder,dtm_test_desc_tfidf)
+final_desc_test_32<-sparseMatrixPredict(dtm_test_desc_tfidf,final_desc_encoder,batch_size=1024,3460725)
 
 model_test_data<-as.matrix(model_test_data)
-final_test_data<-cbind(model_test_data,final_brand_train_8,final_cate_train_8,final_name_train_8,final_desc_train_32)
+final_test_data<-cbind(model_test_data,final_brand_test_8,final_cate_test_8,final_name_test_8,final_desc_test_32)
 final_test_data<-as.matrix(final_test_data)
-save(final_test_data,file="/processed_data/final_test_data.RData")
-
+save(final_test_data,file="processed_data/final_test_data.RData")
+rm(dtm_test_desc,dtm_test_desc_tfidf,final_brand_test_8,final_cate_test_8,final_desc_test_32,final_name_test_8)
 
 ##boolean feature####
 logical_test_data<-apply(final_test_data, 2, as.logical)
 logical_test_data<-apply(logical_test_data,2,as.numeric)
 save(logical_test_data,file="processed_data/logical_test_data.RData")
-
+rm(final_test_data,logical_test_data)
 
 
 
 # stemming feature####
-stem_desc_encoder<-keras:load_model_hdf5("model/stem_desc_encoder_32.h5")
-stem_name_encoder<-keras:load_model_hdf5("model/stem_name_encoder_8.h5")
-stem_cate_encoder<-keras:load_model_hdf5("model/stem_cate_encoder_8.h5")
-stem_brand_encoder<-keras:load_model_hdf5("model/stem_brand_encoder_8.h5")
-load("model/stem_brand_tfidf.RData")
-load("model/stem_cate_tfidf.RData")
-load("model/stem_desc_tfidf.RData")
-load("model/stem_name_tfidf.RData")
-load("model/stem_brand_vectorizer.RData")
-load("model/stem_cate_vectorizer.RData")
-load("model/stem_desc_vectorizer.RData")
-load("model/stem_name_vectorizer.RData")
+stem_desc_encoder<-keras::load_model_hdf5("model/stem_desc_encoder_32.h5")
+stem_name_encoder<-keras::load_model_hdf5("model/stem_name_encoder_8.h5")
+stem_cate_encoder<-keras::load_model_hdf5("model/stem_cate_encoder_8.h5")
+stem_brand_encoder<-keras::load_model_hdf5("model/stem_brand_encoder_8.h5")
+load("processed_data/stem_feature/stem_brand_tfidf.RData")
+load("processed_data/stem_feature/stem_cate_tfidf.RData")
+load("processed_data/stem_feature/stem_desc_tfidf.RData")
+load("processed_data/stem_feature/stem_name_tfidf.RData")
+load("processed_data/stem_feature/stem_brand_vectorizer.RData")
+load("processed_data/stem_feature/stem_cate_vectorizer.RData")
+load("processed_data/stem_feature/stem_desc_vectorizer.RData")
+load("processed_data/stem_feature/stem_name_vectorizer.RData")
 stemming<-function(x){
   c<-paste(unlist(text_tokens(x, stemmer = "en")),collapse = " ")
   c
@@ -198,7 +211,9 @@ it_name= itoken(test_data_name_stem,
 
 dtm_test_stem_name = create_dtm(it_name, stem_name_vectorizer)
 test_stem_name_tfidf = transform(dtm_test_stem_name, stem_name_tfidf)
-stem_test_name_8<-predict_on_batch(stem_name_encoder,test_stem_name_tfidf)
+stem_name_test_8<-sparseMatrixPredict(test_stem_name_tfidf,stem_name_encoder,batch_size=1024,3460725)
+save(stem_name_test_8,file="processed_data/stem_name_test_8.RData")
+rm(dtm_test_stem_name,test_stem_name_tfidf)
 
 
 #brand name
@@ -206,7 +221,7 @@ test_data_brand_stem<-sapply(test_data$brand,stemming)
 prep_fun = tolower
 tok_fun = space_tokenizer
 #name_iterator
-it_brand= itoken(test_data_brant_stem, 
+it_brand= itoken(test_data_brand_stem, 
                 preprocessor = prep_fun, 
                 tokenizer = tok_fun, 
                 ids = test_data$test_id, 
@@ -215,7 +230,9 @@ it_brand= itoken(test_data_brant_stem,
 
 dtm_test_stem_brand = create_dtm(it_brand, stem_brand_vectorizer)
 test_stem_brand_tfidf = transform(dtm_test_stem_brand, stem_brand_tfidf)
-stem_test_brand_8<-predict_on_batch(stem_brand_encoder,test_stem_brand_tfidf)
+stem_brand_test_8<-sparseMatrixPredict(test_stem_brand_tfidf,stem_brand_encoder,batch_size=1024,3460725)
+save(stem_brand_test_8,file="processed_data/stem_brand_test_8.RData")
+rm(dtm_test_stem_brand,test_stem_brand_tfidf)
 
 
 #description
@@ -232,7 +249,9 @@ it_desc= itoken(test_data_desc_stem,
 
 dtm_test_stem_desc = create_dtm(it_desc, stem_desc_vectorizer)
 test_stem_desc_tfidf = transform(dtm_test_stem_desc, stem_desc_tfidf)
-stem_test_desc_32<-predict_on_batch(stem_desc_encoder,test_stem_desc_tfidf)
+stem_desc_test_32<-sparseMatrixPredict(test_stem_desc_tfidf,stem_desc_encoder,batch_size=1024,3460725)
+save(stem_desc_test_32,file="processed_data/stem_desc_test_32.RData")
+rm(dtm_test_stem_desc,test_stem_desc_tfidf)
 
 
 test_data_cate_stem<-sapply(test_data$sub_category2,stemming)
