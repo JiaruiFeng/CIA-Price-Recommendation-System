0b027d094cf0d5dabce5286cd4e9e78843b5b082
JiaruiFeng <feng.jiarui@wustl.edu>
2020-05-03 10:52:35 -0500

    Create ridge_regression.R

---
+++ ridge_regression.R
@@ -0,0 +1,40 @@
+#ridge regression
+source("utils.R")
+#load test result for four model
+load()
+prediction_DNN<-read.delim("processed_data/predictions/DNN_prediction.txt",header=F)
+prediction_CNN<-read.delim("processed_data/predictions/CNN_prediction.txt",header=F)
+dataset<-cbind(prediction_DNN,prediction_CNN,prediction_combine,prediction_logical,prediction_model,prediction_stem)
+colnames(dataset)<-c("dnn","cnn","combine","logical","final","stem")
+dataset<-as.matrix(dataset)
+train_price<-log(true_price+1)
+final_train_price<-train_price[test_index]
+
+
+library(glmnet)
+grid = 10^seq(10,-2,length=100)
+cv.ridge = cv.glmnet(dataset,final_train_price,alpha=0,lambda=grid,intercept=T)
+plot(cv.ridge)
+bestlam = cv.ridge$lambda.min
+ridge_model<-glmnet(dataset,final_train_price,alpha = 0,intercept = T)
+prediction_ridge<-predict(ridge_model,newx=dataset,s=bestlam)
+test_rmsle<-rmsle(prediction_ridge,true_price[test_index])
+predict(ridge_model,x=dataset,type="coefficients",s=bestlam,excat=T)
+
+
+library(caret)
+library(xgboost)
+xb_train_index<-sample(c(1:444499),444499*0.8)
+traindata <- list(data=dataset[xb_train_index,],label=final_train_price[xb_train_index]) 
+dtrain <- xgb.DMatrix(data = traindata$data, label = traindata$label) 
+
+xgb_model <- xgboost(data = dtrain, 
+                     eta = 0.4,
+                     max_depth = 4, 
+                     nround=25
+)
+
+xb_test_prediction<-predict(xgb_model,dataset[-xb_train_index,])
+final_true_price<-true_price[test_index]
+rmsle(xb_test_prediction,final_true_price[-xb_train_index])
+
